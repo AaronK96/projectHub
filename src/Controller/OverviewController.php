6cda36b3e2c1f686fc30edb54e88399ce8d264d5
Aaron Kaschwich <aaron.kaschwich@datagraphis.de>
2026-06-03 13:26:36 +0200

    [TASK] fill Overview with real data

--- src/Controller/OverviewController.php
+++ src/Controller/OverviewController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Project;
+use App\Entity\Task;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,10 +15,43 @@ final class OverviewController extends AbstractController
     public function index(EntityManagerInterface $entityManager): Response
     {
         $projects = $entityManager->getRepository(Project::class)->findAll();
+        $tasks = $entityManager->getRepository(Task::class)->findAll();
+
+        //$openTasks = $entityManager->getRepository(Task::class)->findBy([
+        //    'status' => ['active', 'review']
+        //]);
+
+        $taskCount = [];
+
+        $taskActiveCount = 0;
+        $taskReviewCount = 0;
+        $taskDoneCount = 0;
+
+        foreach ($tasks as $task) {
+            switch ($task->getStatus()) {
+                case 'active':
+                    $taskActiveCount++;
+                    break;
+                case 'review':
+                    $taskReviewCount++;
+                    break;
+                case 'done':
+                    $taskDoneCount++;
+                    break;
+            }
+        }
+
+        $taskCount['active'] = $taskActiveCount;
+        $taskCount['review'] = $taskReviewCount;
+        $taskCount['done'] = $taskDoneCount;
+        $taskCount['open'] = $taskActiveCount + $taskReviewCount;
+
+        dump($taskCount);
 
         return $this->render('overview/index.html.twig', [
             'controller_name' => 'OverviewController',
             'projects' => $projects,
+            'taskCount' => $taskCount,
         ]);
     }
 }
